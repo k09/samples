9f009c246a0e1309203daa2fb0182fb96971b2bb
k09 <1416648+k09@users.noreply.github.com>
2024-10-12 23:07:46 +0000

    Prefixed all filenames with k09

--- generate_file_list.py
+++ generate_file_list.py
@@ -4,17 +4,12 @@ from pathlib import Path
 
 def generate_file_list():
     file_dict = {}
-
-    # Walk through all the files in the repository directory.
     file_dict[f"_base"] = "https://raw.githubusercontent.com/k09/samples/main/"
     for root, dirs, files in os.walk("."):
         for file in files:
-            # Check if file does not end with .wav or .mp3
             if not file.endswith(('.wav', '.mp3')):
                 continue
             file_dict[f"k09_{Path(file).stem}"] = [file]
-
-    # Write the file list to a JSON file
     with open("strudel.json", "w") as f:
         json.dump(file_dict, f, indent=4)
 
