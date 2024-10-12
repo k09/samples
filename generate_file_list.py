afcd7727f71225816aaa9bce27fb4297f11ac6b1
k09 <1416648+k09@users.noreply.github.com>
2024-10-12 10:59:01 +0000

    Changed github url

--- generate_file_list.py
+++ generate_file_list.py
@@ -6,7 +6,7 @@ def generate_file_list():
     file_dict = {}
 
     # Walk through all the files in the repository directory.
-    file_dict[f"_base"] = "https://raw.githubusercontent.com/k09/samples/main/"
+    file_dict[f"_base"] = "https://github.com/k09/samples/main/"
     for root, dirs, files in os.walk("."):
         for file in files:
             # Check if file does not end with .wav or .mp3
